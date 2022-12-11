7c958d2e808104ef89f961fdf72b01ffeb6e9292
michal-mindconsulting <michal.jakubowski@mindconsulting.pl>
2022-12-11 21:57:01 +0100

    Docker image save

--- .docker/octane/docker-entrypoint.sh
+++ .docker/octane/docker-entrypoint.sh
@@ -5,15 +5,14 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     composer install
+    npm install chokidar
     php artisan storage:link
     php artisan key:generate
     php artisan migrate
-    php artisan db:seed
     php artisan config:clear
     php artisan route:clear
 else
     composer install
-    php artisan migrate
     php artisan config:clear
     php artisan route:clear
 fi
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TestController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/test', [TestController::class, 'index']);
+Route::get('/rest', [TestController::class, 'test']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
